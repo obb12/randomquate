449cacabe1d288f67f32cd396ca319194dd01718
unknown <you@example.com>
2019-09-21 06:21:19 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component  {
+  constructor(props) {
+    super(props);
+     this.state = {
+    item:{},
+    items:[
+    {
+      author: 'Ghandi',
+      text: '“Live as if you were to die tomorrow. Learn as if you were to live forever.”'
+    }, {
+      author: 'Steve Jobs',
+      text: '“You can’t connect the dots looking forward; you can only connect them looking backward. So you have to trust that the dots will somehow connect in your future.”'
+    }
+  ]
+     }
+
+  }
+  componentDidMount(){
+    var item = this.state.items[Math.floor(Math.random()*this.state.items.length)];
+    this.setState({ item:item })
+
+  }
+   activateLasers() {
+   var item = this.state.items[Math.floor(Math.random()*this.state.items.length)];
+   this.setState({ item:item })
+ }
+
+    render() {
+
+          return (
+                  <div id="quote-box">
+
+              <div id="text">{this.state.item.text}</div>
+              <div id="author">{this.state.item.author}</div>
+              <button onClick={this.activateLasers.bind(this)} id="new-quote">new quote</button>
+              <a id="tweet-quote" href="https://twitter.com/intent/tweet">Tweet</a>
+                  </div>
+                )
+        }
+};
 
 export default App;
